99afe07324e0af38d2dfe376412be3b287feeee6
Diego Goncalvez <diego.data.icloud@gmail.com>
2020-08-19 07:24:58 -0300

    arme componentes

---
+++ src/components/Pelicula.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function Pelicula() {
+  return (
+    <div>
+        <h2>Titulo de la Pelicula</h2>
+        <p>Rating</p>
+    </div>
+  );
+}
+
+export default Pelicula;
