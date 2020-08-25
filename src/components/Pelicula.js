559417c2e07d20d7eb4b39a539282ba52718d464
Diego Goncalvez <diego.data.icloud@gmail.com>
2020-08-25 07:53:33 -0300

    usando PropTypes y defaultProps

--- src/components/Pelicula.js
+++ src/components/Pelicula.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 //Aca estamos extendiendo nuestra caja de herramientas
 //usando JSX  como lenguaje de renderizacion de 
@@ -17,13 +18,23 @@ function Pelicula(props) {
 //nuestro JSX, con variables, impresiones, repeticiones,
 //y condicionales(donde herramientas como el if ternario 
 //se vuelve sumamente comodas para utilizar con REACT)
-  return (
+let listadoDeGeneros;
+
+if(props.generos !=null){
+	listadoDeGeneros=
+	<ul>
+    { props.generos.map((genero, i)=><li key={genero + i} >{genero}</li>)}
+  </ul>
+}  else{
+  listadoDeGeneros="";
+}
+
+return (
     <div>
         <h2 className="sarasa">{props.titulo}</h2>
         <p>Rating: {props.rating}</p>
-        <ul>
-            { props.generos.map((genero, i)=><li key={genero + i} >{genero}</li>)}
-        </ul>
+        {listadoDeGeneros}
+        
     </div>
   );
 }
@@ -31,5 +42,17 @@ function Pelicula(props) {
 //que esta entre llaves
 //En REACT, en vez de usar un for, se usa map
 //para hacer repeticiones
+
+//****Aca conocemos a DEFAULTPROPS
+Pelicula.defaultProps={
+    rating:"Esta peli aun no fue puntuada!!",
+    generos: null
+}
+
+// eslint-disable-next-line react/no-typos
+Pelicula.propTypes={
+    rating: PropTypes.number,
+    titulo: PropTypes.string
+}
 
 export default Pelicula;
