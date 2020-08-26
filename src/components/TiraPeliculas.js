542feb2d883e0d058534790570fa61fe3ff19555
Diego Goncalvez <diego.data.icloud@gmail.com>
2020-08-26 07:09:19 -0300

    Eventos en React

--- src/components/TiraPeliculas.js
+++ src/components/TiraPeliculas.js
@@ -11,9 +11,27 @@ import Pelicula from './Pelicula';
 //luego reemplazamos la palabra function por class
 //y luego aplicamos el metodo render al return
 class TiraPeliculas extends Component {
+//Aca yo defino las consecuencias que quiero que sucedan cuando
+//sucede el evento.ahora, como los asocio a lugares concretos
+//de uestro html?
+  lanzarAlerta(){
+      alert("Me clickeaste!");
+  }
+
+  cambiarAmarillo(){
+      document.querySelector("body").style.backgroundColor="yellow"
+  }
+
+  cambiarRosa(){
+    document.querySelector("body").style.backgroundColor="Pink"
+  }
+// en el lugar que quiero que suceda, pongo el evento
+//si al evento le pongo parentesis de digo que se ejecute ya, si no
+//le pongo se ejecuta cuando pase el mouse
    render(){ 
     return (
-        <div>
+        <div onClick={this.lanzarAlerta} onMouseOver={this.cambiarAmarillo} onMouseOut={this.cambiarRosa}>
+
             <Pelicula generos={["Fantasia","Aventuras"]} rating={8.0} titulo="Harry Potter" />
             <Pelicula generos={["Animacion","Familiar"]} rating={7.5} titulo="Toy Story" />
             <Pelicula generos={["Drama"]} rating={1.5} titulo="Her" />
